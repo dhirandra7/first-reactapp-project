2734b668f423093cf93e11b054cbc4e6a900ce9c
dhirandra7 <dhirandrapandey7@gmail.com>
2023-10-07 00:10:37 +0530

    inisilize project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react"
 
 function App() {
+
+const [count, setCount] = useState(0)
+ 
+function handlecount(){
+  const val=count +1;
+  setCount(val);
+
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      hello word
+      {
+        count
+      }
+      <button onClick={handlecount}>click me</button>
     </div>
   );
 }
